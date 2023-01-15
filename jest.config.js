3cb21c7fc0b948fe0128a179cafb554fb0f2eaf2
gdnarciso <gdnarciso@rd.com.br>
2023-01-15 20:56:12 -0300

    chore: config paths jest and typescript

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
-module.exports = {
+/** @type {import('jest').Config} */
+const config = {
     moduleFileExtensions: ['js', 'json', 'ts'],
-    rootDir: 'src',
+    rootDir: './',
     testRegex: '.*\\.spec\\.ts$',
     transform: {
         '^.+\\.(t|j)s$': ['@swc/jest'],
@@ -8,4 +9,9 @@ module.exports = {
     collectCoverageFrom: ['**/*.(t|j)s'],
     coverageDirectory: '../coverage',
     testEnvironment: 'node',
+    moduleNameMapper: {
+        '@app(.*)': '<rootDir>/src/app/$1',
+        '@test(.*)': '<rootDir>/test/$1',
+    },
 };
+module.exports = config;
