bc1c7e15a51f6757dd4b166ad659c6ef98c04841
GuilhermeDelfino <guilherme.narciso@sptech.school>
2023-01-16 18:48:07 -0300

    feat(usecases): create readNotification

---
+++ src/app/useCases/readNotification.ts
@@ -0,0 +1,26 @@
+import { NotificationRepository } from '@app/repositories/NotificationRepository';
+import { Injectable } from '@nestjs/common';
+import { NotificationNotFoundError } from './errors/NotificationNotFoundError';
+export type ReadNotificationRequest = {
+    notificationId: string;
+};
+export type ReadNotificationResponse = void;
+@Injectable()
+export class ReadNotification {
+    constructor(private repo: NotificationRepository) {}
+
+    async execute(
+        request: ReadNotificationRequest
+    ): Promise<ReadNotificationResponse> {
+        const { notificationId } = request;
+        const notification = await this.repo.findById(notificationId);
+
+        if (!notification || notification === null) {
+            throw new NotificationNotFoundError();
+        }
+
+        notification.read();
+
+        await this.repo.update(notification);
+    }
+}
