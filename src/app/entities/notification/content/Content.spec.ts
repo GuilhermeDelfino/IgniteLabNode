a5e3558df5397a6023c00f45a425b293ced606ed
gdnarciso <gdnarciso@rd.com.br>
2023-01-16 09:00:42 -0300

    test(entity): content entity tests

---
+++ src/app/entities/notification/content/Content.spec.ts
@@ -0,0 +1,27 @@
+import {
+    JustStringError,
+    MaxLengthError,
+    MinLengthError,
+} from '@app/validations/errors/StringErrors';
+import { Content } from '.';
+
+describe('Entity: Notification Content', () => {
+    it('should be able to create a content', () => {
+        expect(() => Content.of('Conteudo')).toBeTruthy();
+        expect(Content.of('Conteudo').value).toBe('Conteudo');
+        expect(() => Content.of('Conteudo')).not.toThrow();
+    });
+    it('should be throw MinLengthError', () => {
+        expect(() => Content.of('C'.repeat(Content.MIN_LENGTH - 1))).toThrow(
+            MinLengthError
+        );
+    });
+    it('should be throw MaxLengthError', () => {
+        expect(() => Content.of('C'.repeat(Content.MAX_LENGTH + 1))).toThrow(
+            MaxLengthError
+        );
+    });
+    it('should be throw MaxLengthError', () => {
+        expect(() => Content.of('Guilherme 123')).toThrow(JustStringError);
+    });
+});
