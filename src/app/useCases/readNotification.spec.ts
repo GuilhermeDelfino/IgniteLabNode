b9714b16d3d5fa15e9e719835c577b2a99cf06e6
GuilhermeDelfino <guilherme.narciso@sptech.school>
2023-01-16 18:48:36 -0300

    test(usecases): create readNotification `test`

---
+++ src/app/useCases/readNotification.spec.ts
@@ -0,0 +1,27 @@
+import { NotificationRepository } from '@app/repositories/NotificationRepository';
+import { createNotification } from '@test/factories/notificationFactory';
+import { NotificationRepositoryInMemory } from '@test/repositories/NotificationRepositoryInMemory';
+import { NotificationNotFoundError } from './errors/NotificationNotFoundError';
+import { ReadNotification } from './readNotification';
+
+describe('Use case: Read Notification', () => {
+    let repo: NotificationRepository;
+    beforeEach(async () => {
+        repo = new NotificationRepositoryInMemory();
+        await repo.create(createNotification({ id: 'new-id' }));
+    });
+    it('should be able to Read a notification', async () => {
+        const useCase = new ReadNotification(repo);
+        await useCase.execute({ notificationId: 'new-id' });
+        expect(
+            (repo as NotificationRepositoryInMemory).database[0].readAt
+        ).toEqual(expect.any(Date));
+    });
+    it('should be not able to Read a notification', async () => {
+        const useCase = new ReadNotification(repo);
+        expect(
+            async () =>
+                await useCase.execute({ notificationId: 'new-id-incorrect' })
+        ).rejects.toThrow(NotificationNotFoundError);
+    });
+});
