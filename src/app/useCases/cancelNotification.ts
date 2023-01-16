678135456e62bbc7f5493f5abf74719a4b54a114
GuilhermeDelfino <guilherme.narciso@sptech.school>
2023-01-16 17:40:54 -0300

    feat(usecases): create cancelNotification `usecase`

---
+++ src/app/useCases/cancelNotification.ts
@@ -0,0 +1,27 @@
+import { Notification } from '@app/entities/notification';
+import { NotificationRepository } from '@app/repositories/NotificationRepository';
+import { Injectable } from '@nestjs/common';
+import { NotificationNotFoundError } from './errors/NotificationNotFoundError';
+export type CancelNotificationRequest = {
+    notificationId: string;
+};
+export type CancelNotificationResponse = void;
+@Injectable()
+export class CancelNotification {
+    constructor(private repo: NotificationRepository) {}
+
+    async execute(
+        request: CancelNotificationRequest
+    ): Promise<CancelNotificationResponse> {
+        const { notificationId } = request;
+        const notification = await this.repo.findById(notificationId);
+
+        if (!notification || notification === null) {
+            throw new NotificationNotFoundError();
+        }
+
+        notification.cancel();
+
+        await this.repo.update(notification);
+    }
+}
