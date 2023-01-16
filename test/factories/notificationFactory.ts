381da564a6f715f4da1c4bd4e42561ee61ee1ac4
gdnarciso <gdnarciso@rd.com.br>
2023-01-16 10:17:38 -0300

    fix(factories): fix to notification factory set category enum

--- test/factories/notificationFactory.ts
+++ test/factories/notificationFactory.ts
@@ -1,9 +1,10 @@
 import { Notification, NotificationProps } from '@app/entities/notification';
+import { Categories } from '@app/entities/notification/category';
 
 export const createNotification = (props?: Partial<NotificationProps>) => {
     return new Notification({
         ...props,
-        category: 'category',
+        category: Categories.SIMPLE,
         content: 'content',
         recipientId: 'recipient-id',
     });
