8dfc832d0504112d082fc02987d6894e9519004d
gdnarciso <gdnarciso@rd.com.br>
2023-01-16 10:40:04 -0300

    test(entity): added get methods tests

--- src/app/entities/notification/notification.spec.ts
+++ src/app/entities/notification/notification.spec.ts
@@ -5,4 +5,12 @@ describe('Entity: Notification', () => {
         expect(() => createNotification()).toBeTruthy();
         expect(() => createNotification()).not.toThrow();
     });
+    it('should be able to get attributes', () => {
+        expect(createNotification().category).toBe('Simple');
+        expect(createNotification().content).toBe('content');
+        expect(createNotification().recipientId).toBe('recipient-id');
+        expect(createNotification().createdAt).toEqual(expect.any(Date));
+        expect(createNotification().readAt).toEqual(null);
+        expect(createNotification().id).toEqual(expect.any(String));
+    });
 });
