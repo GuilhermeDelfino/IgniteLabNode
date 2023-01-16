8c636588f92e5d0fa379f49ae18b4413eb89b04a
gdnarciso <gdnarciso@rd.com.br>
2023-01-16 10:44:01 -0300

    feat(entity): added notification attributes and get value content

--- src/app/entities/notification/index.ts
+++ src/app/entities/notification/index.ts
@@ -1,18 +1,29 @@
 import { randomUUID } from 'crypto';
+import { Categories } from './category';
+import { Content } from './content';
 
 export type NotificationProps = {
     id?: string;
     recipientId: string;
     content: string;
-    category: string;
+    category: Categories;
+    readAt?: Date | null;
+    createdAt: Date;
+};
+export type NotificationAttributes = {
+    id?: string;
+    recipientId: string;
+    content: Content;
+    category: Categories;
     readAt?: Date | null;
     createdAt: Date;
 };
 export class Notification {
-    private props: Required<NotificationProps>;
+    private props: Required<NotificationAttributes>;
     constructor(props: Omit<NotificationProps, 'readAt' | 'createdAt'>) {
         this.props = {
             ...props,
+            content: Content.of(props.content),
             id: props.id || randomUUID(),
             createdAt: new Date(),
             readAt: null,
@@ -23,7 +34,7 @@ export class Notification {
         return this.props.id;
     }
     get content() {
-        return this.props.content;
+        return this.props.content.value;
     }
     get category() {
         return this.props.category;
