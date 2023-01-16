37e8d604e3d8b039b2334abcdf36d1d8ee7ab8ab
GuilhermeDelfino <guilherme.narciso@sptech.school>
2023-01-16 18:27:54 -0300

    feat(repository): create method findManyByRecipientId in `notification repository`

--- src/app/repositories/NotificationRepository.ts
+++ src/app/repositories/NotificationRepository.ts
@@ -1,8 +1,11 @@
 import { Notification } from '@app/entities/notification';
 
 export abstract class NotificationRepository {
-    abstract create(notification: Notification): Promise<void>;
     abstract findById(notificationId: string): Promise<Notification | null>;
-    abstract update(notification: Notification): Promise<void>;
+    abstract findManyByRecipientId(
+        recipientId: string
+    ): Promise<Notification[]>;
     abstract countManyByRecipientId(recipientId: string): Promise<number>;
+    abstract create(notification: Notification): Promise<void>;
+    abstract update(notification: Notification): Promise<void>;
 }
