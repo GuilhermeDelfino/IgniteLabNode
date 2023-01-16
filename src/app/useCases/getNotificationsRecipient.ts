428b93d41004bac23b38fa48f0e6448834ce3e0d
GuilhermeDelfino <guilherme.narciso@sptech.school>
2023-01-16 18:29:54 -0300

    feat(usecases): create getNotificationRecipient `usecase`

---
+++ src/app/useCases/getNotificationsRecipient.ts
@@ -0,0 +1,24 @@
+import { Notification } from '@app/entities/notification';
+import { NotificationRepository } from '@app/repositories/NotificationRepository';
+import { Injectable } from '@nestjs/common';
+export type GetNotificationRecipientRequest = {
+    recipientId: string;
+};
+export type GetNotificationRecipientResponse = {
+    notificationsByRecipient: Notification[];
+};
+@Injectable()
+export class GetNotificationsRecipient {
+    constructor(private repo: NotificationRepository) {}
+    async execute(
+        request: GetNotificationRecipientRequest
+    ): Promise<GetNotificationRecipientResponse> {
+        const { recipientId } = request;
+        const notifications = await this.repo.findManyByRecipientId(
+            recipientId
+        );
+        return {
+            notificationsByRecipient: notifications,
+        };
+    }
+}
