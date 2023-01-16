d229a450d35d0bcc20436dd7cdaa615bb65387ac
GuilhermeDelfino <guilherme.narciso@sptech.school>
2023-01-16 17:41:15 -0300

    test(usecases): create cancelNotification `tests`

---
+++ src/app/useCases/cancelNotification.spec.ts
@@ -0,0 +1,27 @@
+import { NotificationRepository } from '@app/repositories/NotificationRepository';
+import { createNotification } from '@test/factories/notificationFactory';
+import { NotificationRepositoryInMemory } from '@test/repositories/NotificationRepositoryInMemory';
+import { CancelNotification } from './cancelNotification';
+import { NotificationNotFoundError } from './errors/NotificationNotFoundError';
+
+describe('Use case: Cancel Notification', () => {
+    let repo: NotificationRepository;
+    beforeEach(async () => {
+        repo = new NotificationRepositoryInMemory();
+        await repo.create(createNotification({ id: 'new-id' }));
+    });
+    it('should be able to Cancel a notification', async () => {
+        const useCase = new CancelNotification(repo);
+        await useCase.execute({ notificationId: 'new-id' });
+        expect(
+            (repo as NotificationRepositoryInMemory).database[0].canceledAt
+        ).toEqual(expect.any(Date));
+    });
+    it('should be not able to Cancel a notification', async () => {
+        const useCase = new CancelNotification(repo);
+        expect(
+            async () =>
+                await useCase.execute({ notificationId: 'new-id-incorrect' })
+        ).rejects.toThrow(NotificationNotFoundError);
+    });
+});
