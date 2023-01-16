a493305919b614dea7ee404f34f6be5d2053924c
gdnarciso <gdnarciso@rd.com.br>
2023-01-16 08:58:25 -0300

    fix(test): function get value

--- src/app/validations/string/index.ts
+++ src/app/validations/string/index.ts
@@ -9,6 +9,7 @@ export interface IStringValidator {
     validateValueHasMin(min: number): IStringValidator;
     validateValueHasMax(max: number): IStringValidator;
     validateIsJustString(): IStringValidator;
+    get value(): string;
 }
 export class StringValidator implements IStringValidator {
     private defaultMinLengthMessageError =
@@ -16,23 +17,27 @@ export class StringValidator implements IStringValidator {
     private defaultMaxLengthMessageError = 'The value must be less than max';
     private defaultJustStringMessageError = 'The value must be only a string';
 
-    constructor(private value: string) {}
+    constructor(private _value: string) {}
     validateValueHasMin(min: number): IStringValidator {
-        if (this.value.length < min) {
+        if (this._value.length < min) {
             throw new MinLengthError(this.defaultMinLengthMessageError);
         }
         return this;
     }
     validateValueHasMax(max: number): IStringValidator {
-        if (this.value.length > max) {
+        if (this._value.length > max) {
             throw new MaxLengthError(this.defaultMaxLengthMessageError);
         }
         return this;
     }
     validateIsJustString(): IStringValidator {
-        if (!regexJustString.test(this.value)) {
+        if (!regexJustString.test(this._value)) {
             throw new JustStringError(this.defaultJustStringMessageError);
         }
         return this;
+    }
+
+    get value() {
+        return this._value;
     }
 }
