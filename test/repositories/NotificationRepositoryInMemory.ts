14f457cd323d5e785a294e645f88466aa807267b
GuilhermeDelfino <guilherme.narciso@sptech.school>
2023-01-16 17:39:30 -0300

    feat(repository): implements methods findById and update on `notification repository in memory`

--- test/repositories/NotificationRepositoryInMemory.ts
+++ test/repositories/NotificationRepositoryInMemory.ts
@@ -3,6 +3,17 @@ import { NotificationRepository } from '@app/repositories/NotificationRepository
 
 export class NotificationRepositoryInMemory extends NotificationRepository {
     database: Notification[] = [];
+    async findById(notificationId: string): Promise<Notification | null> {
+        const notification = this.database.find((n) => n.id === notificationId);
+        if (!notification) return null;
+        return notification;
+    }
+    async update(notification: Notification): Promise<void> {
+        const index = this.database.findIndex((n) => n.id === notification.id);
+        if (index >= 0) {
+            this.database[index] = notification;
+        }
+    }
     async create(notification: Notification): Promise<void> {
         this.database.push(notification);
     }
