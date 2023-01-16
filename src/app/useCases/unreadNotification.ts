e9044cc30c38255c78ae688697597a587fc51f60
GuilhermeDelfino <guilherme.narciso@sptech.school>
2023-01-16 18:49:03 -0300

    feat(usecases): create unreadNotification

---
+++ src/app/useCases/unreadNotification.ts
@@ -0,0 +1,26 @@
+import { NotificationRepository } from '@app/repositories/NotificationRepository';
+import { Injectable } from '@nestjs/common';
+import { NotificationNotFoundError } from './errors/NotificationNotFoundError';
+export type UnreadNotificationRequest = {
+    notificationId: string;
+};
+export type UnreadNotificationResponse = void;
+@Injectable()
+export class UnreadNotification {
+    constructor(private repo: NotificationRepository) {}
+
+    async execute(
+        request: UnreadNotificationRequest
+    ): Promise<UnreadNotificationResponse> {
+        const { notificationId } = request;
+        const notification = await this.repo.findById(notificationId);
+
+        if (!notification || notification === null) {
+            throw new NotificationNotFoundError();
+        }
+
+        notification.unread();
+
+        await this.repo.update(notification);
+    }
+}
