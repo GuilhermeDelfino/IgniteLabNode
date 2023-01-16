ed35d2e940354551e9c5f316e36882e29a9e729c
gdnarciso <gdnarciso@rd.com.br>
2023-01-16 10:38:29 -0300

    fix(entity): category entity to just enum

--- src/app/entities/notification/category/index.ts
+++ src/app/entities/notification/category/index.ts
@@ -4,17 +4,3 @@ export enum Categories {
     ALERT = 'Alert',
     RELEVANT = 'Relevant',
 }
-export class Category {
-    private _value: Categories;
-
-    private constructor(category: Categories) {
-        this._value = category;
-    }
-    static of(category: Categories): Category {
-        return new Category(category);
-    }
-
-    get value() {
-        return this._value;
-    }
-}
