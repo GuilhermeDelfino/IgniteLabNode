21a26a19c2965906fd6b56094888cfe319c60361
GuilhermeDelfino <guilherme.narciso@sptech.school>
2023-01-16 22:46:14 -0300

    feat(mapper): create a view-model to show in response controller

---
+++ src/infra/http/view-models/notificationViewModel.ts
@@ -0,0 +1,41 @@
+import { Notification } from '@app/entities/notification';
+import { DateFormatter } from '@helpers/date/DateFormatter';
+
+export type NotificationToResponseAllFieldsFormattedResponse = {
+    notificationId: string;
+    recipientId: string;
+    createdAt: string;
+    category: string;
+    content: string;
+    readedAt?: string;
+    canceledAt?: string;
+};
+export class NotificationViewModel {
+    static notificationToResponseAllFieldsFormatted(
+        notification: Notification
+    ): NotificationToResponseAllFieldsFormattedResponse {
+        const {
+            canceledAt,
+            category,
+            content,
+            createdAt,
+            readedAt,
+            recipientId,
+            id,
+        } = notification;
+
+        return {
+            category,
+            content,
+            createdAt: DateFormatter.formatDateTime(createdAt),
+            notificationId: id,
+            recipientId,
+            canceledAt:
+                canceledAt !== null
+                    ? DateFormatter.formatDateTime(canceledAt)
+                    : '',
+            readedAt:
+                readedAt !== null ? DateFormatter.formatDateTime(readedAt) : '',
+        };
+    }
+}
