bf4d717225cc8e42c9880875c18405adb9a91b0a
GuilhermeDelfino <guilherme.narciso@sptech.school>
2023-01-16 21:22:38 -0300

    feat(http): create readNotification route

--- src/infra/http/controllers/notification.controller.ts
+++ src/infra/http/controllers/notification.controller.ts
@@ -1,10 +1,28 @@
+import { ReadNotification } from '@app/useCases/readNotification';
 import { SendNotification } from '@app/useCases/sendNotification';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApiParam } from '@nestjs/swagger';
 import { SendNotificationDto } from '../dtos/SendNotificationDto';
 
 @Controller('notification')
 export class NotificationController {
-    constructor(private readonly sendNotificationUseCase: SendNotification) {}
+    constructor(
+        private readonly sendNotificationUseCase: SendNotification,
+        private readonly readNotificationUseCase: ReadNotification
+    ) {}
+
+    @ApiParam({
+        name: 'notificationId',
+        example: 'notification-id',
+        required: true,
+    })
+    @Get('read/:notificationId')
+    async readNotification(
+        @Param('notificationId')
+        notificationId: string
+    ) {
+        await this.readNotificationUseCase.execute({ notificationId });
+    }
     @Post('send')
     async sendNotification(@Body() body: SendNotificationDto): Promise<void> {
         await this.sendNotificationUseCase.execute(body);
