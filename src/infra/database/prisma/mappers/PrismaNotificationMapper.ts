74a11c0f4cab2dc318df556202577d4f27819faf
GuilhermeDelfino <guilherme.narciso@sptech.school>
2023-01-16 21:58:47 -0300

    fix(mapper): fixing to map readedAt and canceledAt

--- src/infra/database/prisma/mappers/PrismaNotificationMapper.ts
+++ src/infra/database/prisma/mappers/PrismaNotificationMapper.ts
@@ -4,27 +4,43 @@ import { Notifications as PrismaNotification } from '@prisma/client';
 
 export class PrismaNotificationMapper {
     static domainToPrisma(notification: Notification): PrismaNotification {
-        const { category, content, createdAt, id, recipientId, readAt } =
-            notification;
+        const {
+            category,
+            content,
+            createdAt,
+            id,
+            recipientId,
+            readedAt,
+            canceledAt,
+        } = notification;
         return {
             category,
             content,
             createdAt,
-            readAt,
+            readedAt,
+            canceledAt,
             id,
             recipientId,
         };
     }
     static prismaToDomain(notification: PrismaNotification): Notification {
-        const { category, content, createdAt, id, recipientId, readAt } =
-            notification;
+        const {
+            category,
+            content,
+            createdAt,
+            id,
+            recipientId,
+            canceledAt,
+            readedAt,
+        } = notification;
         return new Notification({
             content,
             recipientId,
             id,
             category: Categories[category],
             createdAt,
-            readAt,
+            readedAt,
+            canceledAt,
         });
     }
 }
