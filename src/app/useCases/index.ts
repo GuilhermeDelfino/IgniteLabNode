aaed22a0a7b13a88b092526fb54d1953fb75fe86
GuilhermeDelfino <guilherme.narciso@sptech.school>
2023-01-16 21:20:32 -0300

    feat(usecases): index to facility the use cases imports

---
+++ src/app/useCases/index.ts
@@ -0,0 +1,15 @@
+import { SendNotification } from './sendNotification';
+import { ReadNotification } from './readNotification';
+import { UnreadNotification } from './unreadNotification';
+import { CountNotificationsRecipient } from './countNotificationsRecipient';
+import { GetNotificationsRecipient } from './getNotificationsRecipient';
+import { CancelNotification } from './cancelNotification';
+
+export {
+    SendNotification,
+    CancelNotification,
+    CountNotificationsRecipient,
+    GetNotificationsRecipient,
+    ReadNotification,
+    UnreadNotification,
+};
