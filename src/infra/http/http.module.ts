f01352ca56caebb72cca169f72c2d4d09f0a442d
GuilhermeDelfino <guilherme.narciso@sptech.school>
2023-01-16 21:21:24 -0300

    feat(http): import all use cases in http module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,4 +1,11 @@
-import { SendNotification } from '@app/useCases/sendNotification';
+import {
+    ReadNotification,
+    SendNotification,
+    CancelNotification,
+    CountNotificationsRecipient,
+    GetNotificationsRecipient,
+    UnreadNotification,
+} from '@app/useCases';
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { NotificationController } from './controllers/notification.controller';
@@ -6,6 +13,13 @@ import { NotificationController } from './controllers/notification.controller';
 @Module({
     imports: [DatabaseModule],
     controllers: [NotificationController],
-    providers: [SendNotification],
+    providers: [
+        ReadNotification,
+        SendNotification,
+        CancelNotification,
+        CountNotificationsRecipient,
+        GetNotificationsRecipient,
+        UnreadNotification,
+    ],
 })
 export class HttpModule {}
