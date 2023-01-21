2e8b5b8cacbc93694271e416d9075b4a17174083
GuilhermeDelfino <guilherme.narciso@sptech.school>
2023-01-21 20:19:33 -0300

    chore: config bull with redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { HttpModule } from './infra/http/http.module';
 
 @Module({
-    imports: [HttpModule],
+    imports: [
+        HttpModule,
+        BullModule.forRoot({
+            redis: {
+                host: 'localhost',
+                port: 6379,
+            },
+        }),
+    ],
 })
 export class AppModule {}
