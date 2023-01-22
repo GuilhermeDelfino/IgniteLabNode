dd7bdff5811a6f2eaa291356770b6e3b6cb6922b
GuilhermeDelfino <guilherme.narciso@sptech.school>
2023-01-21 21:06:42 -0300

    chore: config bull module with .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,7 @@
-import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
 import { HttpModule } from './infra/http/http.module';
 
 @Module({
-    imports: [
-        HttpModule,
-        BullModule.forRoot({
-            redis: {
-                host: 'localhost',
-                port: 6379,
-            },
-        }),
-    ],
+    imports: [HttpModule],
 })
 export class AppModule {}
---
+++ src/infra/queues/bull/QueueBull.module.ts
@@ -0,0 +1,27 @@
+import { NotificationsJobs } from '@app/jobs/NotificationJobs';
+import { BullModule } from '@nestjs/bull';
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from '@src/infra/database/database.module';
+import { NotificationsBullJobs } from './jobs/NotificationsBullJob';
+import { NotificationBullProcessor } from './processors/NotificationBullProcessor';
+
+@Module({
+    imports: [
+        BullModule.forRoot({
+            redis: {
+                host: process.env.QUEUE_HOST,
+                port: Number(process.env.QUEUE_PORT),
+            },
+        }),
+        BullModule.registerQueue({
+            name: 'notification',
+        }),
+        DatabaseModule,
+    ],
+    providers: [
+        { provide: NotificationsJobs, useClass: NotificationsBullJobs },
+        NotificationBullProcessor,
+    ],
+    exports: [NotificationsJobs],
+})
+export class QueueBullModule {}
